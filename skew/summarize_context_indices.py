0d5119aad8f1e13911adb9bfc1c467d16b5b3592
KwatME <kwatme8@gmail.com>
2017-12-13 19:10:46 -0800

    19

--- skew/summarize_context_indices.py
+++ skew/summarize_context_indices.py
@@ -3,19 +3,20 @@ from numpy import sign
 
 def summarize_context_indices(
         context_indices,
-        summarize_context_indices_method='summarize_only_shape_side',
+        summarize_context_indices_method='summarize_shape_side',
         shape=None):
     """
     Summarize context indices.
     Arguments:
         context_indices (array): (n)
-        summarize_context_indices_method (str): 'summarize_only_shape_side'
+        summarize_context_indices_method (str): 'summarize_shape_side' |
+            'summarize_both_side'
         shape (float):
     Returns:
         float: context summary
     """
 
-    if summarize_context_indices_method == 'summarize_only_shape_side':
+    if summarize_context_indices_method == 'summarize_shape_side':
         return ((sign(context_indices) == sign(shape)) * context_indices).sum()
 
     elif summarize_context_indices_method == 'summarize_both_side':
