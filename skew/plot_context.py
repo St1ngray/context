3f9a5186b50779045c471500ff077295d1a6814e
KwatME <kwatme8@gmail.com>
2017-12-13 18:55:11 -0800

    2

--- skew/plot_context.py
+++ skew/plot_context.py
@@ -13,7 +13,7 @@ from .plot.plot.style import FIGURE_SIZE
 
 def plot_context(array_1d,
                  title,
-                 figure_size=FIGURE_SIZE,
+                 figure_size=[FIGURE_SIZE[0] * 1.8, FIGURE_SIZE[1]],
                  n_bin=None,
                  plot_skew_t_pdf=True,
                  plot_context_indices=True,
