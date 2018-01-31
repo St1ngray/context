a95fce3ef758da17887ecfbe3a1c0450ae54a3e1
KwatME <kwatme8@gmail.com>
2018-01-31 01:14:19 -0800

    refactored with shift

--- context/compute_context.py
+++ context/compute_context.py
@@ -1,9 +1,25 @@
-from numpy import absolute, argmax, argmin, array, linspace, log, sqrt, where
+from numpy import (absolute, argmax, argmin, array, linspace, log, sign, sqrt,
+                   where, zeros_like)
 from statsmodels.sandbox.distributions.extras import ACSkewT_gen
 
 from .fit_skew_t_pdf import fit_skew_t_pdf
 from .nd_array.nd_array.get_coordinates_for_reflection import \
     get_coordinates_for_reflection
+
+
+def get_intersections_between_2_1d_arrays(array_2d_0, array_2d_1):
+    """
+    Get intersections between 2 1D arrays.
+    Arguments:
+        array_2d_0 (ndarray): (n, )
+        array_2d_1 (ndarray): (n, )
+    Returns:
+        ndarray: (n, ); bools
+    """
+
+    from numpy import append, diff
+
+    return append(diff(sign(array_2d_0 - array_2d_1)) != 0, False)
 
 
 def compute_context(array_1d,
@@ -15,8 +31,10 @@ def compute_context(array_1d,
                     fit_fixed_location=None,
                     fit_fixed_scale=None,
                     n_grid=3000,
-                    true_mean=None,
-                    degrees_of_freedom_for_tail_reduction=10e8):
+                    degrees_of_freedom_for_tail_reduction=10e8,
+                    global_location=None,
+                    global_scale=None,
+                    global_shape=None):
     """
     Compute context.
     Arguments:
@@ -29,14 +47,17 @@ def compute_context(array_1d,
         fit_fixed_location (float):
         fit_fixed_scale (float):
         n_grid (int):
-        true_mean (float):
         degrees_of_freedom_for_tail_reduction (float):
+        global_location (float):
+        global_scale (float):
+        global_shape (float):
     Returns:
         dict: {
             fit: ndarray; (5, ) (N, Location, Scale, DF, Shape),
             grid: ndarray; (n_grid, ),
             pdf: ndarray; (n_grid, ),
             pdf_reference: ndarray; (n_grid, ),
+            global_pdf_reference: ndarray; (n_grid, ),
             context_indices: ndarray; (n_grid, ),
             context_indices_like_array: ndarray; (n, ),
             context_summary: float,
@@ -71,31 +92,51 @@ def compute_context(array_1d,
         loc=location,
         scale=scale)
 
-    if true_mean is not None:
+    context_indices = (pdf - pdf_reference) / pdf
+    context_indices[context_indices < 0] = 0
 
-        location_penalties = absolute((grid - true_mean) / location)
+    penalty = log(df) / sqrt(absolute(shape) * scale)
+    context_indices = context_indices**penalty
 
-        lower_boundary = location - 0.5 * scale
-        higher_boundary = location + 0.5 * scale
+    i = argmax(pdf_reference)
+    context_indices = ((-1, ) * i + (1, ) * (n_grid - i)) * context_indices
 
-        if true_mean < lower_boundary or higher_boundary < true_mean:
+    if all(
+            parameter is not None
+            for parameter in (
+                global_location,
+                global_scale,
+                global_shape, )):
 
-            if location < 0:
-                location_penalties = where(grid < higher_boundary,
-                                           location_penalties, 0)
-            else:
-                where(lower_boundary < grid, location_penalties, 0)
+        global_pdf_reference = skew_t_model.pdf(
+            grid,
+            degrees_of_freedom_for_tail_reduction,
+            global_shape,
+            loc=global_location,
+            scale=global_scale)
 
-            pdf_reference = pdf_reference**(1 + location_penalties)
+        global_context_indices = (pdf - global_pdf_reference) / pdf
 
-    grid_for_reflection = argmax(pdf_reference)
+        penalty *= 1 + (scale / global_scale) / (
+            absolute(location - global_location) / global_scale)
 
-    context_indices_magnitude = where(
-        pdf_reference < pdf, ((pdf - pdf_reference) / pdf),
-        0)**(log(df) / sqrt(absolute(shape) * scale))
+        global_context_indices = global_context_indices**penalty
 
-    context_indices = context_indices_magnitude * (
-        (-1, ) * grid_for_reflection + (1, ) * (n_grid - grid_for_reflection))
+        is_intersection = get_intersections_between_2_1d_arrays(
+            pdf, global_pdf_reference)
+
+        if location < global_location:
+            global_context_indices *= grid < grid[is_intersection][0]
+            global_context_indices *= -1
+        else:
+            global_context_indices *= grid[is_intersection][1] < grid
+
+        context_indices = where(
+            absolute(context_indices) < absolute(global_context_indices),
+            global_context_indices, context_indices)
+
+    else:
+        global_pdf_reference = zeros_like(pdf_reference)
 
     context_indices_like_array = context_indices[[
         argmin(absolute(grid - value)) for value in array_1d
@@ -104,11 +145,11 @@ def compute_context(array_1d,
     absolute_value_weighted_context_like_array = absolute(
         array_1d) * context_indices_like_array
 
-    negative_context_summary = absolute_value_weighted_context_like_array[:
-                                                                          grid_for_reflection].sum(
-                                                                          )
+    negative_context_summary = absolute_value_weighted_context_like_array[
+        absolute_value_weighted_context_like_array < 0].sum()
+
     positive_context_summary = absolute_value_weighted_context_like_array[
-        grid_for_reflection:].sum()
+        0 < absolute_value_weighted_context_like_array].sum()
 
     if absolute(negative_context_summary) < absolute(positive_context_summary):
         context_summary = positive_context_summary
@@ -125,6 +166,7 @@ def compute_context(array_1d,
         'grid': grid,
         'pdf': pdf,
         'pdf_reference': pdf_reference,
+        'global_pdf_reference': global_pdf_reference,
         'context_indices': context_indices,
         'context_indices_like_array': context_indices_like_array,
         'context_summary': context_summary,
