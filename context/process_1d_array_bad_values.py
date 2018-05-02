bfc318c4cd883abbaa40dba24bbc2f8c4cc51875
KwatME <kwatme8@gmail.com>
2018-05-02 00:25:19 -0700

    mean

--- context/process_1d_array_bad_values.py
+++ context/process_1d_array_bad_values.py
@@ -1,5 +1,3 @@
-from numpy import nanmean
-
 from .nd_array.nd_array.check_nd_array_for_bad_value import \
     check_nd_array_for_bad_value
 
@@ -16,6 +14,6 @@ def process_1d_array_bad_values(_1d_array):
 
     elif is_bad_value.any():
 
-        _1d_array[is_bad_value] = nanmean(_1d_array)
+        _1d_array[is_bad_value] = _1d_array[~is_bad_value].mean()
 
     return _1d_array
