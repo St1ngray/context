78b1ea9d274d238640aaa409115739a6a54c736d
KwatME <kwatme8@gmail.com>
2018-05-02 00:23:37 -0700

    raise_ = False

--- context/process_1d_array_bad_values.py
+++ context/process_1d_array_bad_values.py
@@ -8,7 +8,7 @@ def process_1d_array_bad_values(_1d_array):
 
     _1d_array = _1d_array.copy()
 
-    is_bad_value = check_nd_array_for_bad_value(_1d_array)
+    is_bad_value = check_nd_array_for_bad_value(_1d_array, raise_=False)
 
     if is_bad_value.all():
 
