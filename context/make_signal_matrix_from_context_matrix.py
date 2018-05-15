c9b23268ec256a6a20734d9278157c8a327a87cd
KwatME <kwatme8@gmail.com>
2018-05-15 03:09:44 -0700

    10

--- context/make_signal_matrix_from_context_matrix.py
+++ context/make_signal_matrix_from_context_matrix.py
@@ -1,6 +1,6 @@
 def make_signal_matrix(_1d_context_matrix, select_context):
 
-    if select_context not in ('negative', 'positive', 'both'):
+    if select_context not in ('negative', 'positive'):
 
         raise ValueError('Unknown select_context: {}.'.format(select_context))
 
@@ -15,9 +15,5 @@ def make_signal_matrix(_1d_context_matrix, select_context):
     elif select_context == 'positive':
 
         _1d_context_matrix[_1d_context_matrix < 0] = 0
-
-    elif select_context == 'both':
-
-        raise NotImplementedError()
 
     return _1d_context_matrix
