6a32ccd658d89c6978ac9153aa7848768a211fbf
KwatME <kwatme8@gmail.com>
2018-06-04 14:46:25 -0700

    Updated make_2d_signal_matrix function to take in manual number of feature or sample

--- context/make_2d_signal_matrix.py
+++ context/make_2d_signal_matrix.py
@@ -5,21 +5,42 @@ from .nd_array.nd_array.normalize_nd_array import normalize_nd_array
 from .summarize_1d_context_matrix import summarize_1d_context_matrix
 
 
-def make_2d_signal_matrix(feature_1d_context_matrix, sample_1d_context_matrix,
-                          select_context):
+def make_2d_signal_matrix(feature_1d_context_matrix,
+                          sample_1d_context_matrix,
+                          select_context,
+                          n_feature=None,
+                          n_sample=None):
 
     feature_context_summary = summarize_1d_context_matrix(
         feature_1d_context_matrix, select_context)
 
-    feature_selected = 'TODO'
+    feature_context_summary = feature_context_summary.abs().sort_values()
+
+    if n_feature is None:
+
+        selected_features = feature_context_summary[
+            feature_context_summary != 0].index
+
+    else:
+
+        selected_features = feature_context_summary[-n_feature:].index
 
     sample_context_summary = summarize_1d_context_matrix(
         sample_1d_context_matrix, select_context)
 
-    sample_selected = 'TODO'
+    sample_context_summary = sample_context_summary.abs().sort_values()
+
+    if n_sample is None:
+
+        selected_samples = sample_context_summary[
+            sample_context_summary != 0].index
+
+    else:
+
+        selected_samples = sample_context_summary[-n_sample:].index
 
     feature_signal_matrix = make_1d_signal_matrix(
-        feature_1d_context_matrix.loc[feature_selected, sample_selected],
+        feature_1d_context_matrix.loc[selected_features, selected_samples],
         select_context)
 
     feature_signal_matrix = DataFrame(
@@ -28,7 +49,7 @@ def make_2d_signal_matrix(feature_1d_context_matrix, sample_1d_context_matrix,
         feature_signal_matrix.index, feature_signal_matrix.columns)
 
     sample_signal_matrix = make_1d_signal_matrix(
-        sample_1d_context_matrix.loc[sample_selected, feature_selected],
+        sample_1d_context_matrix.loc[selected_samples, selected_features],
         select_context)
 
     sample_signal_matrix = DataFrame(
