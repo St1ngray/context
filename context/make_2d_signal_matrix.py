0b98b275b62fcf50f21b24500cb95ed4f538bcb5
KwatME <kwatme8@gmail.com>
2018-06-05 14:24:14 -0700

    0

--- context/make_2d_signal_matrix.py
+++ context/make_2d_signal_matrix.py
@@ -1,60 +1,77 @@
+from numpy import multiply
 from pandas import DataFrame
 
-from .make_1d_signal_matrix import make_1d_signal_matrix
+from ._make_1d_signal_matrix import _make_1d_signal_matrix
+from ._select_elements_by_context_summary import \
+    _select_elements_by_context_summary
 from .nd_array.nd_array.normalize_nd_array import normalize_nd_array
-from .summarize_1d_context_matrix import summarize_1d_context_matrix
 
 
 def make_2d_signal_matrix(feature_1d_context_matrix,
                           sample_1d_context_matrix,
                           select_context,
-                          n_feature=None,
-                          n_sample=None):
+                          features=None,
+                          samples=None,
+                          n_top_feature=None,
+                          n_top_sample=None,
+                          select_automatically=False,
+                          normalization_method='0-1',
+                          combining_function=multiply):
 
-    feature_context_summary = summarize_1d_context_matrix(
-        feature_1d_context_matrix, select_context)
+    if features is None:
 
-    feature_context_summary = feature_context_summary.abs().sort_values()
+        features = _select_elements_by_context_summary(
+            feature_1d_context_matrix,
+            select_context,
+            n_top=n_top_feature,
+            select_automatically=select_automatically)
 
-    if n_feature is None:
+    if samples is None:
 
-        selected_features = feature_context_summary[
-            feature_context_summary != 0].index
+        samples = _select_elements_by_context_summary(
+            sample_1d_context_matrix,
+            select_context,
+            n_top=n_top_sample,
+            select_automatically=select_automatically)
 
-    else:
+    feature_signal_matrix = _make_1d_signal_matrix(
+        feature_1d_context_matrix.loc[features, samples], select_context)
 
-        selected_features = feature_context_summary[-n_feature:].index
+    sample_signal_matrix = _make_1d_signal_matrix(
+        sample_1d_context_matrix.loc[samples, features], select_context)
 
-    sample_context_summary = summarize_1d_context_matrix(
-        sample_1d_context_matrix, select_context)
+    if normalization_method is not None:
 
-    sample_context_summary = sample_context_summary.abs().sort_values()
+        try:
 
-    if n_sample is None:
+            feature_signal_matrix = DataFrame(
+                normalize_nd_array(
+                    feature_signal_matrix.values,
+                    normalization_method,
+                    1,
+                    raise_for_bad_value=False), feature_signal_matrix.index,
+                feature_signal_matrix.columns)
 
-        selected_samples = sample_context_summary[
-            sample_context_summary != 0].index
+        except ValueError as exception:
 
-    else:
+            raise ValueError(
+                'Normalizing feature_signal_matrix failed.\nException: {}.\nTry setting normalization_method=None.'.
+                format(exception))
 
-        selected_samples = sample_context_summary[-n_sample:].index
+        try:
 
-    feature_signal_matrix = make_1d_signal_matrix(
-        feature_1d_context_matrix.loc[selected_features, selected_samples],
-        select_context)
+            sample_signal_matrix = DataFrame(
+                normalize_nd_array(
+                    sample_signal_matrix.values,
+                    normalization_method,
+                    1,
+                    raise_for_bad_value=False), sample_signal_matrix.index,
+                sample_signal_matrix.columns)
 
-    feature_signal_matrix = DataFrame(
-        normalize_nd_array(
-            feature_signal_matrix.values, '0-1', 1, raise_for_bad_value=False),
-        feature_signal_matrix.index, feature_signal_matrix.columns)
+        except ValueError as exception:
 
-    sample_signal_matrix = make_1d_signal_matrix(
-        sample_1d_context_matrix.loc[selected_samples, selected_features],
-        select_context)
+            raise ValueError(
+                'Normalizing sample_signal_matrix failed.\nException: {}.\nTry setting normalization_method=None.'.
+                format(exception))
 
-    sample_signal_matrix = DataFrame(
-        normalize_nd_array(
-            sample_signal_matrix.values, '0-1', 1, raise_for_bad_value=False),
-        sample_signal_matrix.index, sample_signal_matrix.columns)
-
-    return feature_signal_matrix * sample_signal_matrix.T
+    return combining_function(feature_signal_matrix, sample_signal_matrix.T)
